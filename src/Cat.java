40c63fde9ab9a41e7adc62f92648e411ebd14810
DarynaHorobei97 <daryna_horobei1@ukr.net>
2023-06-21 21:03:37 +0300

    initial commit

---
+++ src/Cat.java
@@ -0,0 +1,18 @@
+public class Cat {
+
+    String color;
+    int weight;
+
+    void showCatInfo(){
+        System.out.println("Cat has such characteristics " + color + " " + weight);
+    }
+
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        cat1.color = "black";
+        cat1.weight = 5;
+
+        System.out.println(5/0);
+    }
+
+}
---
+++ src/TestClass.java
@@ -0,0 +1,14 @@
+
+public class TestClass {
+
+    static void sumOfTwoNumbers(int a, int b) {
+        int sum = a + b;
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+       sumOfTwoNumbers(2, 3);
+
+
+    }
+}
